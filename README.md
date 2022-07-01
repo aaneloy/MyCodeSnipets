9db563f0ff087a3862333732d70cc215812f6fec
Asif Ahmed Neloy <asif.neloy@northsouth.edu>
2022-07-01 01:59:22 -0500

    pandas syntax added

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MyCodeSnipets
+![Author](https://img.shields.io/badge/author-aaneloy-blue)
+
+Last time I checked, I forgot my name. Remembering python or pandas syntax is too overrated for me. 
+
+I will write down each every new snippet I learn and will press ````CTRL+C```` and ````CTRL+V```` everytime.
+
+
+:rofl: 
+
+
+
+## License
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ pandas_syntax.py
@@ -0,0 +1,29 @@
+# read csv without unnamed columns
+health_df = pd.read_csv(r'dataset\processed.csv')
+health_df = health_df.loc[:, ~health_df.columns.str.contains('^Unnamed')]
+print(health_df.shape)
+health_df.head(5)
+
+#skip unnamed colums
+df.to_csv(r"dataset\processed.csv", index=False)
+
+# Delete rows containing either 70% or more than 90% NaN Values
+perc = 70.0
+min_count =  int(((100-perc)/100)*df.shape[1] + 1)
+mod_df = df.dropna( axis=0,
+                    thresh=min_count)
+print("Modified Dataframe : ")
+print(mod_df)
+
+
+#Drop Dataframe columns containing 60% missing value
+df_2 = df.dropna(thresh=df.shape[0]*0.6,how='all',axis=1)
+
+#missing value imputer
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(mod_df)
+imputed_df = imputer.transform(mod_df.values)
+
+#drrop rows by range
+health_df = health_df.drop(health_df.index[92064:100766])
